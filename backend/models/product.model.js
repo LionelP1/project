3654d8da104754efc992383a0ada511477f293f4
Lionel Pei <lpei1900@gmail.com>
2025-03-07 21:47:03 -0500

    Created schema for product

---
+++ backend/models/product.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        enum: ['fruits', 'vegetables', 'dairy', 'grains', 'others'],
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        default: 'default.jpg'
+    },
+    farmer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
